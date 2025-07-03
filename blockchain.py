d98b7bb9cde92f2f4fea6d56a794cc91cbf8ca74
imviv1 <vk572487@gmail.com>
2025-07-03 11:37:53 +0530

    Added encrypt_data method

--- blockchain.py
+++ blockchain.py
@@ -6,3 +6,15 @@ class Block:
 
     def calculate_hash(self):
         return "hash"  # Dummy hash for now
+class Block:
+    def __init__(self, data, previous_hash):
+        self.original_data = data
+        self.data = self.encrypt_data(data)  # Encrypt before storing
+        self.previous_hash = previous_hash
+        self.hash = self.calculate_hash()
+
+    def calculate_hash(self):
+        return "hash"  # Dummy hash
+
+    def encrypt_data(self, data):
+        return data + 10  # Dummy encryption (FHE-inspired)
