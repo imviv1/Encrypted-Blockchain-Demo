c9ce4fd9fb10037b8cc3642fd0a3c878ea68cb77
imviv1 <vk572487@gmail.com>
2025-07-03 11:49:01 +0530

    Added MIT License and example usage in blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -33,3 +33,35 @@ class Blockchain:
 # Example usage
 bc = Blockchain()
 bc.add_block("Transaction 1")
+class Block:
+    def __init__(self, data, previous_hash):
+        self.original_data = data
+        self.data = self.encrypt_data(data)  # Encrypt before storing
+        self.previous_hash = previous_hash
+        self.hash = self.calculate_hash()
+
+    def calculate_hash(self):
+        return "hash"  # Dummy hash
+
+    def encrypt_data(self, data):
+        return data + 10  # Dummy encryption (FHE-inspired)
+
+class Blockchain:
+    def __init__(self):
+        self.chain = [self.create_genesis_block()]
+
+    def create_genesis_block(self):
+        return Block("Genesis", "0")
+
+    def add_block(self, data):
+        previous_block = self.chain[-1]
+        new_block = Block(data, previous_block.hash)
+        self.chain.append(new_block)
+
+# Example usage
+if __name__ == "__main__":
+    bc = Blockchain()
+    bc.add_block("Transaction 1")
+    bc.add_block("Transaction 2")
+    for block in bc.chain:
+        print(f"Original Data: {block.original_data}, Encrypted Data: {block.data}, Hash: {block.hash}, Prev Hash: {block.previous_hash}")
