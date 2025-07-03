2f639a639b4e72294a2f80be3c5fc4aa227230da
imviv1 <vk572487@gmail.com>
2025-07-03 11:38:37 +0530

    Added Blockchain class and add_block method

--- blockchain.py
+++ blockchain.py
@@ -18,3 +18,18 @@ class Block:
 
     def encrypt_data(self, data):
         return data + 10  # Dummy encryption (FHE-inspired)
+class Blockchain:
+    def __init__(self):
+        self.chain = [self.create_genesis_block()]
+
+    def create_genesis_block(self):
+        return Block("Genesis", "0")
+
+    def add_block(self, data):
+        previous_block = self.chain[-1]
+        new_block = Block(data, previous_block.hash)
+        self.chain.append(new_block)
+
+# Example usage
+bc = Blockchain()
+bc.add_block("Transaction 1")
