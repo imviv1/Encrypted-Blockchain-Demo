2165f7e3cf718e04f77e663722f64e32e763dcbd
imviv1 <vk572487@gmail.com>
2025-07-03 11:37:07 +0530

    Added basic Block class

---
+++ blockchain.py
@@ -0,0 +1,8 @@
+class Block:
+    def __init__(self, data, previous_hash):
+        self.data = data
+        self.previous_hash = previous_hash
+        self.hash = self.calculate_hash()
+
+    def calculate_hash(self):
+        return "hash"  # Dummy hash for now
